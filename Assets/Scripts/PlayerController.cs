563a96047c464256a749c503f4161b3e46a4c95f
Madhav Paudel <poudelmadhav143@gmail.com>
2023-04-27 18:20:18 +0545

    spawn points and void

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -93,6 +93,11 @@ public class PlayerController : MonoBehaviourPunCallbacks, IDamageable
         {
             items[itemIndex].Use();
         }
+
+        if (transform.position.y < -10f) // Die if you fall out of the world
+        {
+            Die();
+        }
     }
 
     void Look()
