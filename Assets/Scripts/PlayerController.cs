c47cfd75214d773b3077291b2b975df97ced6cf4
Madhav Paudel <poudelmadhav143@gmail.com>
2023-04-27 15:01:16 +0545

    gun setup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,10 @@ public class PlayerController : MonoBehaviour
     [SerializeField] GameObject cameraHolder;
     [SerializeField] float mouseSensitivity, sprintSpeed, walkSpeed, jumpForce, smoothTime;
 
+    [SerializeField] Item[] items;
+    int itemIndex;
+    int previousItemIndex = -1;
+
     float verticalLookRotation;
     bool grounded;
     Vector3 smoothMoveVelocity;
@@ -25,7 +29,11 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-        if(!PV.IsMine)
+        if(PV.IsMine)
+        {
+           EquipItem(0); 
+        }
+        else
         {
             Destroy(GetComponentInChildren<Camera>().gameObject);
             Destroy(rb);
@@ -40,6 +48,15 @@ public class PlayerController : MonoBehaviour
         Look();
         Move();
         Jump();
+
+        for (int i = 0; i < items.Length; i++)
+        {
+            if (Input.GetKeyDown((i + 1).ToString()))
+            {
+                EquipItem(i);
+                break;
+            }
+        }
     }
 
     void Look()
@@ -64,6 +81,23 @@ public class PlayerController : MonoBehaviour
         {
             rb.AddForce(transform.up * jumpForce);
         }
+    }
+
+    void EquipItem(int _index)
+    {
+        if (_index == previousItemIndex)
+            return;
+
+        itemIndex = _index;
+
+        items[itemIndex].itemGameObject.SetActive(true);
+        
+        if (previousItemIndex != -1)
+        {
+            items[previousItemIndex].itemGameObject.SetActive(false);
+        }
+        
+        previousItemIndex = itemIndex;
     }
 
     public void SetGroundedState(bool _grounded)
